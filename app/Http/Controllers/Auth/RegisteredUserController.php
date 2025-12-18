33d8e479eeef9bac623543c42324821e6d08a8ab
suchana-das014 <suchanadas541@gmail.com>
2025-12-18 19:56:58 +0600

    updated registration page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,8 +43,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        return redirect()->route('login')
+        ->with('success', 'Registration successful. Please login.');
 
-        return redirect(route('dashboard', absolute: false));
     }
 }
